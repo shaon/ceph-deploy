163942d77e6ed28800572b5f0ac275996f29ca56
I H Shaon <mdshaonimran@gmail.com>
2014-09-17 18:43:19 +0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'shaon@eucalyptus.com'
 license          'Apache v2'
 description      'cookbook for deploying ceph cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'yum'
